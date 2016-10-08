37e07c495ac6705f69bb60a3d36cc0114d19062e
marco <marcojetson@gmail.com>
2016-10-08 11:06:01 +0200

    use options v2 ui

--- options.js
+++ options.js
@@ -1,10 +1,18 @@
-chrome.storage.sync.get(null, store => {
-  document.querySelectorAll('.option').forEach(input => {
-    input.addEventListener('change', e => {
-      chrome.storage.sync.set({[OPTION_PREFIX + e.target.name]: e.target.value});
-    });
+const OPTION_SELECTOR = '.option';
 
-    input.value = store[OPTION_PREFIX + input.name] || '';
-  });
+const SAVE_SELECTOR = '#save';
+
+let inputs = document.querySelectorAll(OPTION_SELECTOR);
+
+chrome.storage.sync.get(null, store => {
+  inputs.forEach(input => { input.value = store[OPTION_PREFIX + input.name] || ''; });
+});
+
+document.querySelector(SAVE_SELECTOR).addEventListener('click', () => {
+  let options = {};
+  inputs.forEach(input => { options[OPTION_PREFIX + input.name] = input.value; });
+  chrome.storage.sync.set(options);
+
+  close();
 });
 
